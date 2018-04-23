5453b6afe47419204e58f11263fc3fbd18ecd501
manikafsgarg1 <manikafsgarg@gmail.com>
2018-04-23 15:17:00 +0530

    micropost settings'

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ApplicationRecord
 	has_many :microposts
 	validates :name, length: { maximum: 15},    presence: true
-	validates :email, length: { maximum: 20},    presence: true
+	validates :email, length: { maximum: 100},    presence: true
 end
